39a1d34871acf3730a283bbbc6da6fd69f74e0f3
Jill <jill_new@mail.ru>
2015-12-17 15:53:41 +0300

    Добавлена возможность компиляции директорий с пробелами. Signed-off-by: Jill <jill_new@mail.ru>

--- ecomp.bat
+++ ecomp.bat
@@ -1,5 +1,5 @@
 @echo off
 xcopy /T /Y /C .\SRC\_Модули .\_Модули
 for /r .\SRC\_Модули %%i in (*.1s) do (
-call ecompfile.bat %%~dpi
+call ecompfile.bat "%%~dpi"
 )
--- ecompfile.bat
+++ ecompfile.bat
@@ -3,10 +3,10 @@ set "fa=%1"
 set relpath=%CD%
 Setlocal EnableDelayedExpansion
 CALL SET SRC=%%fa:!relpath!=%%
-CALL SET SRC=%%SRC:~0,-1%%
+CALL SET SRC=%%SRC:~1,-2%%
 CALL SET rmDEST=%%fa:!relpath!\SRC=%%
-CALL SET rmDEST=%%rmDEST:~0,-1%%
+CALL SET rmDEST=%%rmDEST:~1,-2%%
 CALL SET DEST=%rmDEST%.ert
-ENDLOCAL & SET "SRC=.%SRC%" & SET "rmDEST=.%rmDEST%" & SET "DEST=.%DEST%"
+ENDLOCAL & SET "SRC=".%SRC%"" & SET "rmDEST=".%rmDEST%"" & SET "DEST=".%DEST%""
 RMDIR /S /Q %rmDEST%
 gcomp -c -F %DEST% -DD %SRC% -vv > ert_compile.log
